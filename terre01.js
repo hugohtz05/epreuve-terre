b1bdd202163ef8317f09cb427258a1c771b964c3
hugohtz <hugoheintz03@gmail.com>
2023-11-16 19:27:57 +0100

    alphabet

--- terre01.js
+++ terre01.js
@@ -1,10 +1,11 @@
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+// fonctions utilisées
 function alphabet() {
   let result = "";
   for (let i = 0; i < letters.length; i++) {
     result += letters[i];
   }
+// affichage  
   console.log(result);
 }
 
