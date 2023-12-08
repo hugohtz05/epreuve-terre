b8882a3f6b97c719f605cbb1308c556054fe952d
hugohtz03 <hugoheintz03@gmail.com>
2023-12-08 12:20:26 +0100

    alphabet a partir de

--- terre04.js
+++ terre04.js
@@ -1,6 +1,6 @@
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Fonction utilisée
+// fonctions utilisées
 function alphabet(startLetter) {
   let startIndex = letters.indexOf(startLetter);
 
@@ -15,7 +15,7 @@ function alphabet(startLetter) {
 let startLetter = process.argv[2];
 
 if (startLetter) {
-  alphabet(startLetter.toLowerCase()); // Convertir en minuscules pour la comparaison
+  alphabet(startLetter.toLowerCase()); 
 } else {
   console.log("erreur");
 }
