a7125bf1b8b24551a8f16565250e4ead2bb766b0
hugohtz03 <hugoheintz03@gmail.com>
2023-12-10 16:27:10 +0100

    Puissance d'un nombre

--- terre09.js
+++ terre09.js
@@ -1,14 +1,13 @@
 let argument = process.argv[2];
-let exposant = process.argv[3];
-let resultat;
-
+let exponent = process.argv[3];
+let increment = 0;
+let result = 1; 
 function powerOfNumber() {
-  if (exposant >= 0) {
-    resultat = Math.pow(argument, exposant);
-    console.log(resultat);
-  } else {
-    console.log("erreur");
-  }
+    while (increment < exponent) {
+        increment ++;
+        result = result * argument;  
+    }
+    console.log(result);
 }
 
 powerOfNumber();
